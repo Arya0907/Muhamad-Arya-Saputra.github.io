bc900c7b42f48ae0b9cbab6dd9b5eb7d17ea4c15
Arya0907 <grizzmamba5@gmail.com>
2023-11-22 09:29:42 +0700

    upload pertama

---
+++ 1.js
@@ -0,0 +1,7 @@
+hamburger = document.querySelector(".hamburger");
+hamburger .onclick = function () {
+    navbar = document.querySelector(".nav-bar")
+    navbar.classList.toggle("active")
+}
+
+alert("Selamat Datang di Website saya")
